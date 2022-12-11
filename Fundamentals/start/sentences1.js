2cad70ef0d5cac586569d279346594a20acf7665
Antonio Filho <Antonio.Filho@fit-tecnologia.org.br>
2022-12-11 00:56:31 -0300

    Primeiro commit, Fundamentos JS com  Sentença de código 1 e 2

---
+++ Fundamentals/start/sentences1.js
@@ -0,0 +1,9 @@
+// Primeira aula prendemos de  'Sentenças'
+// o que é uma senteça ?
+//Uma sentença corresponde a um passo do algoritmo ou a uma declaração. 
+//Se o código fonte descreve um algoritmo, então sentenças correspondem a passos
+// do algoritmo, se não forem declarações. 
+
+console.log('Bom dia!')
+console.log('Boa tarde!!')
+console.log('Boa noite!!!')
---
+++ Fundamentals/start/sentences2.js
@@ -0,0 +1,5 @@
+// console.log é o comando JS utilizado para imprimir informações na tela 
+// Dentro dos () digite o que deseja imprimir 
+// Para numeros nao é necesario aspas mas para letras/palavras sim
+
+console.log('Comando JS utilizado para imprimir informações na tela: console.log ')
