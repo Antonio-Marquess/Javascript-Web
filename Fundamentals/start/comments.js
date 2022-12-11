dccb5fb6d7b856d5624c4358ee14d5399e92cec3
Antonio Filho <Antonio.Filho@fit-tecnologia.org.br>
2022-12-11 01:22:27 -0300

    Comentarios em JS

---
+++ Fundamentals/start/comments.js
@@ -0,0 +1,20 @@
+// Quando iniciamos com  // quer dizer que vamos inserir um comentario no codigo 
+// Exemplos:
+// console.log é usado para exibir algo na tela do computador!
+console.log('Bom dia!')
+//Com  comentario depois da sentença
+console.log('Boa tarde!!') // console.log é usado para exibir algo na tela do computador!
+// com // no inicio da sentença assim essa linha de codigo nao vai funcionar
+//console.log('Boa noite!!!')
+
+
+/*
+ para comentario de 
+ múltiplas linhas!
+ utilizamso: 
+ /*
+ */
+
+*/
+
+
